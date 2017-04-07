a1c47f164e6e1e53808833abb6ea0b895a31ea93
Mike Ryan <mr@mry4n.net>
2017-04-07 07:22:32 -0400

    added methods to primeUtility

--- src/collectors/primeUtility.java
+++ src/collectors/primeUtility.java
@@ -1,6 +1,10 @@
 package collectors;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import static java.util.stream.Collectors.*;
 import java.util.stream.IntStream;
 
 public class primeUtility {
@@ -8,6 +12,28 @@ public class primeUtility {
 	public static boolean isPrime(int candidate){
 		return IntStream.range(2, candidate)
 				.noneMatch(i -> candidate % i == 0);
+	}
+	
+	public static Map<Boolean, List<Integer>> partitionPrimes(int n) {
+		return IntStream.rangeClosed(2, n).boxed()
+				.collect(partitioningBy(candidate -> isPrime(candidate)));
+	}
+	
+	public static boolean isPrimeImproved(int candidate){
+		int candidateRoot = (int) Math.sqrt((double) candidate);
+		return IntStream.rangeClosed(2,  candidateRoot)
+				.noneMatch(i -> candidate % i == 0);
+	}
+	
+	public static <A> List<A> takeWhile(List<A> list, Predicate<A> p){
+		int i=0;
+		for (A item : list){
+			if (!p.test(item)){
+				return list.subList(0, i);
+			}
+			i++;
+		}
+		return list;
 	}
 	
 	//TODO
